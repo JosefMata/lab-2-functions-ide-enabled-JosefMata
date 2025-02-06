f561d886c9344a25ec5c9c168f9d583bf93b1522
JosefMata <josef.mata01@utrgv.edu>
2025-02-05 23:56:14 -0600

    Finished Lab2.

--- FunctionsLab.cpp
+++ FunctionsLab.cpp
@@ -11,7 +11,13 @@ using namespace std; // making life easier so that we do not need to use std::ci
 
 
 int locateSmallest(int array[], int start, int end){
-	return -1; // fix this!
+	 int smallestIndex = start;
+    for (int i = start + 1; i <= end; i++) {
+        if (array[i] < array[smallestIndex]) {
+            smallestIndex = i;
+        }
+    }
+	return smallestIndex; // fix this!
 }
 
 //Write the function called replaceVariable in the given interval such that 
@@ -20,8 +26,12 @@ int locateSmallest(int array[], int start, int end){
 //     an int which is the starting index of search,
 //     an int which is the ending point of searchspace
 // function: Search in the array from given start to end index and replace the given target value with 1000
-void replaceVariable() {
-	return;
+void replaceVariable(int array[], int target, int start, int end) {
+    for (int i = start; i <= end; i++) {
+        if (array[i] == target) {
+            array[i] = 1000;
+        }
+    }
 }
 
 
@@ -29,8 +39,11 @@ void replaceVariable() {
 //write a function called printArray to print out the elements of the given array
 //output: nothing
 //input: the array of integers and its size
-void printArray() {
-	return;
+void printArray(int array[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
 }
 
 
@@ -48,6 +61,12 @@ void printArray() {
 //     n%m gives you the remainder after dividing n to m. for example 7%3 = 1 because 7 = 3*2 + 1, then 1 is the remainder.
 
 
-int sumOdds(int array[], int start, int end){
-	return -1;
+int sumOdds(int array[], int start, int end) {
+    int sum = 0;
+    for (int i = start; i <= end; i++) {
+        if (array[i] % 2 != 0) {
+            sum += array[i];
+        }
+    }
+    return sum;
 }
